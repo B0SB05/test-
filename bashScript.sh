9c3b79dae1506f74d49bf7ef1abeffeb81148f69
Muhammed M.Bassem <mbassem@allisootak.com>
2014-01-18 15:26:12 +0200

    Testing bash Script Upload

---
+++ bashScript.sh
@@ -0,0 +1,16 @@
+#! /bin/bash 
+echo -n "Host IP Address : "
+read hostIP
+echo " ----------- TCP SYNC Ping ----------------"
+nmap -PS $hostIP  -oX $hostIP"Sync"
+echo " ----------- TCP ACK Ping ----------------"
+nmap -PA $hostIP  -oX $hostIP"ACK"
+echo " ----------- ARP Ping ----------------"
+nmap -PR $hostIP -oX $hostIP"ARP"
+echo " ----------- Echo Request Ping ----------------"
+nmap -PE $hostIP  -oX $hostIP"EchoRequest"
+echo " ----------- UDP Ping ----------------"
+nmap -PU $hostIP  -oX $hostIP"UDP"
+
+
+
